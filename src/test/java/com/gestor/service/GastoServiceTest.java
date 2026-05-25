a22f17697a8812a1f60f5afdcc290521cff7ad87
Rony <ronygaleana3103@gmail.com>
2026-05-25 07:38:14 -0600

    test: implementar primera prueba unitaria para GastoService usando JUnit 5 y Mockito

--- src/test/java/com/gestor/service/GastoServiceTest.java
+++ src/test/java/com/gestor/service/GastoServiceTest.java
@@ -7,7 +7,7 @@ import com.gestor.model.CategoriaKakeibo;
 import com.gestor.repository.GastoRepository;
 import com.gestor.repository.UsuarioRepository;
 
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -33,7 +33,7 @@ public class GastoServiceTest {
     @Mock
     private UsuarioRepository usuarioRepo;
 
-    @InjectMock
+    @InjectMocks
     private GastoService gastoService;
 
     private Gasto gastoMock;
@@ -44,7 +44,7 @@ public class GastoServiceTest {
         gastoMock = new Gasto();
         gastoMock.setId(1L);
         gastoMock.setDescripcion("Quincena Mayo");
-        gastoMock.setMonto("4500.0");
+        gastoMock.setMonto(4500.0);
         gastoMock.setTipo(TipoTransaccion.INGRESO);
         gastoMock.setCategoria(CategoriaKakeibo.SUPERVIVENCIA);
         gastoMock.setFecha(LocalDate.now());
@@ -53,6 +53,20 @@ public class GastoServiceTest {
    @Test
    @DisplayName("Debería retornar lista de DTOs filtrados correctamente por mes")
    void filtrarPorPeriodoMesExitoso(){
-        //1.GIVEN
+        //1.GIVEN(Dado que el repositorio ficticio devuelve este comportamiento)
+        String email = "test@correo.com";
+        when(gastoRepo.buscarGastosPorEmailUsuario(email)).thenReturn(Arrays.asList(gastoMock));
+
+        //2.WHEN(Cuando ejecutamos la lógica de negocio que queremos probar)
+        List<GastoResponseDTO> resultado = gastoService.filtrarPorPeriodo(email,"mes");
+
+        //3.THEN(Afirmamos y verificamos que el resultado sea el esperado)
+        assertNotNull(resultado);
+        assertEquals(1, resultado.size());
+        assertEquals("Quincena Mayo", resultado.get(0).getDescripcion());
+        assertEquals(4500.0, resultado.get(0).getMonto());
+
+        //Verificamos que el servicio realmente haya llamado al repositorio una sola vez
+        verify(gastoRepo, times(1)).buscarGastosPorEmailUsuario(email);
    }
 }
