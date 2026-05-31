119fc5485794951cdbc2f37976afa9de85108ce6
Rony <ronygaleana3103@gmail.com>
2026-05-30 22:32:42 -0600

    feat:Se han terminado de agregar anotaciones al DTO de MetaAhorro

--- src/main/java/com/gestor/dto/MetaAhorroRequestDTO.java
+++ src/main/java/com/gestor/dto/MetaAhorroRequestDTO.java
@@ -1,26 +1,29 @@
 package com.gestor.dto;
 
 import com.gestor.model.MetaAhorro;
+import javax.validation.constraints;
 
 public class MetaAhorroRequestDTO {
-
+    
+    @NotNull(message= "El monto es obligatorio")
     private Double montoObjetivo;
-    private String fechaCreacion;
+    @NotBlank(message="El mes y anio no debe estar vacío")
+    private String mesAnio;
 
     //Constructor vacío
     public MetaAhorroRequestDTO(){}
 
 
     //Constructor con parámetros
-    public MetaAhorroRequestDTO(Double montoObjetivo, String fechaCreacion){
+    public MetaAhorroRequestDTO(Double montoObjetivo, String mesAnio){
     this.montoObjetivo = montoObjetivo;
-    this.fechaCreacion = fechaCreacion;
+    this.mesAnio = mesAnio;
     }
 
     //Getters y Setters
     public Double getMontoObjetivo(){ return montoObjetivo; }
     public void setMontoObjetivo(Double montoObjetivo){ this.montoObjetivo = montoObjetivo;}
 
-    public String getFechaCreacion(){ return fechaCreacion; }
-    public void setFechaCreacion(String fechaCreacion){ this.fechaCreacion = fechaCreacion; }
+    public String getMesAnio(){ return mesAnio; }
+    public void setMesAnio(String mesAnio){ this.mesAnio = mesAnio; }
 }
